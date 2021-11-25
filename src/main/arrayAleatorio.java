561f553b71772c472c283cb4cf8b5ef15d55fa84
Fran Silva <fransilvamartin@gmail.com>
2021-11-25 14:33:29 +0100

    Commit 1

---
+++ src/main/arrayAleatorio.java
@@ -0,0 +1,19 @@
+package main;
+
+public class arrayAleatorio {
+
+	public static void main(String[] args) {
+		// Crea un método en java para insertar en un array 20 números aleatorios entre 1 y 10
+		
+		int array[] = new int[20];
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math. random()*10+1);
+		}
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+
+}

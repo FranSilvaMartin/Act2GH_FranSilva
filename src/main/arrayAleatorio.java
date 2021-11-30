27806e9d35e28fe22663429d58cf9c22e321eddb
Fran Silva <fransilvamartin@gmail.com>
2021-11-30 13:05:02 +0100

    Añadido método de contar números repetido en una array

--- src/main/arrayAleatorio.java
+++ src/main/arrayAleatorio.java
@@ -1,25 +1,50 @@
 package main;
 
+import java.util.Arrays;
+
 public class arrayAleatorio {
 
 	public static void main(String[] args) {
-		// Crea un método en java para insertar en un array 20 números aleatorios entre 1 y 10
-		
+		// Crea un método en java para insertar en un array 20 números aleatorios entre
+		// 1 y 10
+
 		int array[] = new int[20];
-		
+
 		for (int i = 0; i < array.length; i++) {
-			array[i] = (int)(Math. random()*10+1);
+			array[i] = (int) (Math.random() * 10 + 1);
 		}
-		
+
 		imprimir(array);
+		contarNumeros(array);
 	}
-	
+
 	public static void imprimir(int[] array) {
-		
+
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(i + ",");
 		}
-		
+
+	}
+
+	public static void contarNumeros(int[] array) {
+
+		Arrays.sort(array);
+		int contador = 0;
+		int aux = array[0];
+
+		System.out.println("");
+		for (int i = 0; i < array.length; i++) {
+
+			if (aux == array[i]) {
+				contador++;
+			} else {
+				System.out.println("El número " + aux + " se repite " + contador + " veces");
+				contador = 1;
+				aux = array[i];
+			}
+		}
+		System.out.println("El número " + aux + " se repite " + contador + " veces");
+
 	}
 
 }
